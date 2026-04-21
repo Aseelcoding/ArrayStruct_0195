9aa6d44a7be17c510cc09e8d39d6b94c66425e8d
Aseel <asilalhashmi@email.com>
2026-04-21 10:02:33 +0700

    Copy and paste from the past program

---
+++ Source1.cpp
@@ -0,0 +1,42 @@
+
+
+#include <iostream>
+using namespace std;
+
+struct StAddress
+{
+	string village;
+	string city;
+};
+struct Student
+{
+	string NIM;
+	string Name;
+	StAddress  Address;
+	int Age;
+};
+int main()
+{
+	Student S1;
+	cout << "Please Enter Your NIM ? : "; cin >> S1.NIM;
+	cout << endl;
+
+	cout << "Please Enter Your Name ? : "; cin >> S1.Name;
+	cout << endl;
+
+	cout << "Please Enter Your City ? : "; cin >> S1.Address.city;
+	cout << endl;
+	cout << "Please Enter Your village ? : "; cin >> S1.Address.village;
+	cout << endl;
+
+	cout << "Please Enter Your Age ? : "; cin >> S1.Age;
+	cout << endl;
+
+	cout << "NIM : " << S1.NIM << endl;
+	cout << "Name : " << S1.Name << endl;
+	cout << "City : " << S1.Address.city << endl;
+	cout << "village : " << S1.Address.village << endl;
+	cout << "Age : " << S1.Age << endl;
+
+}
+
