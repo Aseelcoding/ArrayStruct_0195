34ffe599891f1485ae735ccdfa176406d27dd893
Aseel <asilalhashmi@email.com>
2026-04-21 09:58:48 +0700

    add the address struct

---
+++ Source.cpp
@@ -0,0 +1,39 @@
+
+
+#include <iostream>
+using namespace std;
+
+struct Address
+{
+	string village;
+	string city;
+};
+struct Student
+{
+	string NIM;
+	string Name;
+	string Address;
+	int Age;
+};
+int main()
+{
+	Student S1;
+	cout << "Please Enter Your NIM ? : "; cin >> S1.NIM;
+	cout << endl;
+
+	cout << "Please Enter Your Name ? : "; cin >> S1.Name;
+	cout << endl;
+
+	cout << "Please Enter Your Adress ? : "; cin >> S1.Address;
+	cout << endl;
+
+	cout << "Please Enter Your Age ? : "; cin >> S1.Age;
+	cout << endl;
+
+	cout << "NIM : " << S1.NIM << endl;
+	cout << "Name : " << S1.Name << endl;
+	cout << "Address : " << S1.Address << endl;
+	cout << "Age : " << S1.Age << endl;
+
+}
+
